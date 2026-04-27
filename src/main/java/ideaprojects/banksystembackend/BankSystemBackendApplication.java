4f5911b44e5a0a407dfb7a494f9f08a9ee8ebda1
240103158 <240103158@sdu.edu.kz>
2026-04-27 14:42:43 +0500

    first commit

---
+++ src/main/java/ideaprojects/banksystembackend/BankSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ideaprojects.banksystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystemBackendApplication.class, args);
+    }
+
+}
