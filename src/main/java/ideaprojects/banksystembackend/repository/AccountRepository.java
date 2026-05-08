3b26922a122ef04cd889680edb9462852093d51c
240103158 <240103158@sdu.edu.kz>
2026-05-08 15:58:12 +0500

    Added:Custom find methods

--- src/main/java/ideaprojects/banksystembackend/repository/AccountRepository.java
+++ src/main/java/ideaprojects/banksystembackend/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package ideaprojects.banksystembackend.repository;
 
 import ideaprojects.banksystembackend.Entity.Account;
+import ideaprojects.banksystembackend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    Account findByUser(User user);
+    Account findByUserId(int userId);
 }
--- src/main/java/ideaprojects/banksystembackend/repository/TransactionRepository.java
+++ src/main/java/ideaprojects/banksystembackend/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package ideaprojects.banksystembackend.repository;
 
+import ideaprojects.banksystembackend.Entity.Account;
 import ideaprojects.banksystembackend.Entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    Transaction findByAccount(Account Account);
+    Transaction findByAccountId(int accountId);
+    Transaction findByAccountOrderByDateDesc(Account account);
 }
